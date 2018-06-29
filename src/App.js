0f2e2bfc5adeb93f34ccb0d06ef696fde9011cf3
Alexander Adrian Guerra <alex@paper-rad.io>
2018-06-29 13:15:04 -0500

    Mass commit

--- src/App.js
+++ src/App.js
@@ -5,52 +5,32 @@ import Person from "./Person/Person";
 
 class App extends Component {
     state = {
+        me: {
+            name: "Alexander Adrian Guerra",
+            age: 31,
+            pets: ["Ada"]
+        },
         people: ["alex", "Jordan", "gabe"],
         ideas: ["Make list of tools"],
-        bandsILike: ["Radiohead"]
+        bandsILike: ["Radiohead", "The Beatles", "Devo"]
     };
+
+    loggingButtonHandler = () => {
+        console.log("Boooo!!!");
+        this.setState({});
+    };
+
     render() {
         return (
             <div className="App">
-                <h1>Hello</h1>
+                <h1>Me</h1>
 
-                <p>This is not HTML.</p>
+                <h2>Name: {this.state.me.name}</h2>
+                <h2>Age: {this.state.me.age}</h2>
 
-                <p>Make a notes tag</p>
-
-                <p className="note">This is a note!</p>
-
-                <p className="note">Use this a note book</p>
-
-                <div>
-                    <h2>Bands</h2>
-                    <ul>
-                        <li>{this.state.bandsILike[0]}</li>
-                    </ul>
-                </div>
-
-                <h1>Heros</h1>
-                <ul>
-                    <li>Aquaman</li>
-                    <li>Deadpool</li>
-                    <li>Spider Man</li>
-                </ul>
-
-                <h1>Feelings</h1>
-                <div className="things stuff">
-                    <ul>
-                        <li>Happy</li>
-                        <li>Sad</li>
-                        <li>Scared</li>
-                    </ul>
-                </div>
-
-                <h1>People</h1>
-                <Person name="Alex" />
-                <Person name="Jordan" />
-                <Person name="Gabe"> MEEP!!!</Person>
-
-                <button>Push me...</button>
+                <button onClick={this.loggingButtonHandler}>
+                    Change my name to Alex
+                </button>
             </div>
         );
     }
